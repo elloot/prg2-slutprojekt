8ba8a96f6a8500599e55d2468840d1de8c2f83af
elloot <elliot.duchek@gmail.com>
2021-04-28 20:53:38 +0200

    add logic for reading image from inputstream

--- src/ScreenListener.java
+++ src/ScreenListener.java
@@ -1,2 +1,34 @@
-public class ScreenListener {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ScreenListener implements Runnable {
+    private InputStream in;
+    private boolean running;
+
+    public ScreenListener(InputStream i) {
+        in = i;
+    }
+
+    @Override
+    public void run() {
+        running = true;
+        while(running) {
+            byte[] data;
+            try {
+                data = in.readAllBytes();
+            } catch (IOException e) {
+                data = new byte[0];
+                e.printStackTrace();
+            }
+            BufferedImage screenShot;
+            try {
+                screenShot = ImageIO.read(new ByteArrayInputStream(data));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
