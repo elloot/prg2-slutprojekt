e98d904d137c34ccc1c2278605f68a67a91efb08
elloot <elliot.duchek@gmail.com>
2021-04-28 21:08:51 +0200

    add client ui for displaying server screen

---
+++ src/ClientUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
+public class ClientUI {
+    private JPanel panel;
+    private ImageLabel screen;
+
+    public ClientUI() {
+        screen = new ImageLabel();
+        panel.add(screen);
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(panel);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
