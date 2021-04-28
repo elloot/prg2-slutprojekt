35f50173a9aff5c0266244be34283641febba662
elloot <elliot.duchek@gmail.com>
2021-04-28 19:57:02 +0200

    add serversocket and socket, wait for client connection in constructor

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private ServerSocket serverSocket;
+    private Socket socket;
+
+    public Server(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            socket = serverSocket.accept();
+        } catch (IOException e) {
+            System.out.println("Failed to connect on port " + port);
+            e.printStackTrace();
+        }
+    }
 }
