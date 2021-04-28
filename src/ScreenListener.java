4d951b58061ffd2a0e87cbb72b5fb62fe32978e9
elloot <elliot.duchek@gmail.com>
2021-04-28 21:18:06 +0200

    exit process if it can't read the image or convert the bytes to an image

--- src/ScreenListener.java
+++ src/ScreenListener.java
@@ -23,14 +23,18 @@ public class ScreenListener implements Runnable {
                 data = in.readAllBytes();
             } catch (IOException e) {
                 data = new byte[0];
+                System.out.println("Failed to read image from server, exiting");
                 e.printStackTrace();
+                System.exit(0);
             }
             BufferedImage screenShot;
             try {
                 screenShot = ImageIO.read(new ByteArrayInputStream(data));
             } catch (IOException e) {
                 screenShot = new BufferedImage(0, 0, BufferedImage.TYPE_INT_RGB);
+                System.out.println("Failed to convert bytes to image, exiting");
                 e.printStackTrace();
+                System.exit(0);
             }
             client.updateScreen(screenShot);
         }
