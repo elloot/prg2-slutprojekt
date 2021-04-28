faa53b9e96f7107569ee803ce9d8a845cb6eeaf0
elloot <elliot.duchek@gmail.com>
2021-04-28 20:30:53 +0200

    add rectangle for screen bounds and robot for controlling the pc

--- src/ScreenStreamer.java
+++ src/ScreenStreamer.java
@@ -1,10 +1,21 @@
+import java.awt.*;
 import java.io.OutputStream;
 
 public class ScreenStreamer implements Runnable {
     private OutputStream out;
+    private Rectangle screenBounds;
+    private Robot robot;
 
     public ScreenStreamer(OutputStream o) {
         out = o;
+        screenBounds = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            System.out.println("Failed to initialise robot, exiting");
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
     @Override
     public void run() {
