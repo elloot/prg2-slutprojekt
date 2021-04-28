cbebe6b3961692874fd96c2dd08d378fab93eb12
elloot <elliot.duchek@gmail.com>
2021-04-28 20:33:42 +0200

    add logic for taking and sending screenshot

--- src/ScreenStreamer.java
+++ src/ScreenStreamer.java
@@ -1,4 +1,7 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.io.OutputStream;
 
 public class ScreenStreamer implements Runnable {
@@ -19,6 +22,13 @@ public class ScreenStreamer implements Runnable {
     }
     @Override
     public void run() {
-
+        BufferedImage screenshot = robot.createScreenCapture(screenBounds);
+        try {
+            ImageIO.write(screenshot, "JPEG", out);
+        } catch (IOException e) {
+            System.out.println("Failed to send image of screen, exiting");
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
 }
