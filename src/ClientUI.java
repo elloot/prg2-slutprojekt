4bc9b68d3de7e1655cb286934e123bac9f0f7732
elloot <elliot.duchek@gmail.com>
2021-04-28 22:05:13 +0200

    trying to set up ui, currently getting this error Cannot invoke "com.intellij.uiDesigner.core.GridConstraints.getRow()" because "c" is null

--- src/ClientUI.java
+++ src/ClientUI.java
@@ -1,14 +1,21 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.nio.Buffer;
+import java.util.Arrays;
 
 public class ClientUI {
     private JPanel panel;
     private ImageLabel screen;
 
-    public ClientUI() {
-        screen = new ImageLabel();
+    public ClientUI(BufferedImage image) {
+        screen = new ImageLabel(image);
+        screen.setLocation((1920/2)/2, (1080/2) / 2);
         panel.add(screen);
         JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1920/2, 1080/2);
         frame.getContentPane().add(panel);
         frame.setLocationRelativeTo(null);
         frame.pack();
