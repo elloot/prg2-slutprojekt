50bfe37c390e068d001262705adc76373fcc12f8
elloot <elliot.duchek@gmail.com>
2021-05-18 19:38:00 +0200

    rename the main classes

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public class SecondMain {
+public class ClientMain {
     public static void main(String[] args) {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         Client client = new Client(1234, "127.0.0.1", screenSize.width / 2, screenSize.height / 2);
