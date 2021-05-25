f25e40c85d23a01ed5259a65fcedad5ac1a0c4ff
elloot <elliot.duchek@gmail.com>
2021-05-25 12:48:07 +0200

    remove assignment of new Client to variable

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class ClientMain {
     public static void main(String[] args) {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        Client client = new Client(1234, JOptionPane.showInputDialog("Input the ip you wish to connect to"),
+        new Client(1234, JOptionPane.showInputDialog("Input the ip you wish to connect to"),
                 screenSize.width / 2, screenSize.height / 2);
     }
 }
