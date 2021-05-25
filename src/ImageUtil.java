1e6f223ec4cf440e53a0bdc9637a18045cdfc317
elloot <elliot.duchek@gmail.com>
2021-05-25 13:30:54 +0200

    remove redundant methods and subsequent imports

--- src/ImageUtil.java
+++ src/ImageUtil.java
@@ -1,25 +1,7 @@
-import org.imgscalr.Scalr;
-
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class ImageUtil {
-    public static ImageIcon resize(ImageIcon im, int w, int h) {
-        return new ImageIcon(Scalr.resize(toBufferedImage(im), w, h));
-    }
-
-    public static BufferedImage toBufferedImage(ImageIcon icon) {
-        BufferedImage bi = new BufferedImage(
-                icon.getIconWidth(),
-                icon.getIconHeight(),
-                BufferedImage.TYPE_INT_RGB);
-        java.awt.Graphics g = bi.createGraphics();
-        icon.paintIcon(null, g, 0, 0);
-        g.dispose();
-        return bi;
-    }
-
     public static Image getScaledImage(Image srcImg, int w, int h) {
         BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2 = resizedImg.createGraphics();
