79598695c12001cbb07882243d0c22f78ead71de
elloot <elliot.duchek@gmail.com>
2021-05-25 13:20:50 +0200

    add JavaDoc, remove redundant method

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,11 @@ import java.awt.*;
 import java.io.IOException;
 import java.net.Socket;
 
+/**
+ * A controller class for the part of the program that controls the other computer. Sends data between the UI and the
+ * necessary models (i.e. the MouseStreamer and ScreenListener). This class also sets up the connection between it
+ * and the server with a Socket.
+ */
 public class Client {
     private Socket socket;
     private ClientUI ui;
@@ -11,13 +16,21 @@ public class Client {
     private final int initialWidth;
     private final int initialHeight;
 
-    public Client(int port, String ip, int initialWidth, int initialHeight) {
+    /**
+     * Constructs a client with a port and IP to try to connect to and an initial width and height for the UI. When
+     * constructed, the client immediately attempts to connect to the server using the provided IP and port.
+     * @param port The port the client should try to connect on.
+     * @param IP The IP the client should try to connect to.
+     * @param initialWidth The initial width to use for the UI.
+     * @param initialHeight The initial height to use for the UI.
+     */
+    public Client(int port, String IP, int initialWidth, int initialHeight) {
         this.initialWidth = initialWidth;
         this.initialHeight = initialHeight;
         try {
-            socket = new Socket(ip, port);
+            socket = new Socket(IP, port);
         } catch (IOException e) {
-            System.out.println("Failed to connect to ip " + ip + " on port " + port);
+            System.out.println("Failed to connect to IP " + IP + " on port " + port);
             e.printStackTrace();
             System.exit(0);
         }
@@ -40,6 +53,10 @@ public class Client {
         listenerThread.start();
     }
 
+    /**
+     * Updates the UI with the incoming image of the server's screen.
+     * @param im Image of the server's screen.
+     */
     public void updateScreen(ImageIcon im) {
         if (ui == null) {
             ui = new ClientUI(fitToSize(im, initialWidth, initialHeight));
@@ -49,11 +66,28 @@ public class Client {
         ui.updateScreen(fitToSize(im, ui.getBounds().width, ui.getBounds().height));
     }
 
+    /**
+     * Fits an ImageIcon to the specified width and height, i.e. gets the largest width and height that the ImageIcon
+     * can have and still fit inside the specified dimensions.
+     * @param srcIco The ImageIcon that will be used to create a new, fitted ImageIcon.
+     * @param width The width that the new ImageIcon should fit into.
+     * @param height The height that the new ImageIcon should fit into.
+     * @return A new ImageIcon that fits inside the specified width and height.
+     */
     private ImageIcon fitToSize(ImageIcon srcIco, int width, int height) {
         Dimension newSize = calcSizeFromBounds(srcIco, width, height);
         return new ImageIcon(ImageUtil.getScaledImage(srcIco.getImage(), newSize.width, newSize.height));
     }
 
+    /**
+     * Calculates the width and height that an ImageIcon should have to fit inside the specified bounds (width and
+     * height).
+     * @param ico The ImageIcon used to determine proper width and height of a fitted icon.
+     * @param width The width that the ImageIcon needs to fit into.
+     * @param height The height that the ImageIcon needs to fit into.
+     * @return A dimension describing the width and height an ImageIcon should have to fit inside the specified width
+     * and height.
+     */
     private Dimension calcSizeFromBounds(ImageIcon ico, int width, int height) {
         int iconWidth = ico.getIconWidth();
         int iconHeight = ico.getIconHeight();
@@ -65,9 +99,5 @@ public class Client {
             newWidth = (int) (newHeight * aspectRatio);
         }
         return new Dimension(newWidth, newHeight);
-    }
-
-    public ImageIcon resize(ImageIcon im) {
-        return ImageUtil.resize(im, ui.getBounds().width, ui.getBounds().height);
     }
 }
