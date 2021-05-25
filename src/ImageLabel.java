040720d851e3037c170be634efba7f5cffebb194
elloot <elliot.duchek@gmail.com>
2021-05-25 13:29:36 +0200

    add JavaDoc

--- src/ImageLabel.java
+++ src/ImageLabel.java
@@ -1,6 +1,13 @@
 import javax.swing.*;
 
+/**
+ * A wrapper class for the JLabel designed to make using the JLabel for displaying images easier.
+ */
 class ImageLabel extends JLabel {
+    /**
+     * Constructs a new imageLabel with the specified ImageIcon as its icon.
+     * @param icon The ImageIcon that the imageLabel should initially display.
+     */
     public ImageLabel(ImageIcon icon) {
         setIcon(icon);
         setIconTextGap(0);
@@ -9,6 +16,10 @@ class ImageLabel extends JLabel {
         setSize(icon.getImage().getWidth(null), icon.getImage().getHeight(null));
     }
 
+    /**
+     * Sets the icon of the imageLabel.
+     * @param icon The image icon that the imageLabel should display.
+     */
     public void setImage(ImageIcon icon) {
         setIcon(icon);
         setSize(icon.getImage().getWidth(null), icon.getImage().getHeight(null));
