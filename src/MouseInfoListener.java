2bd9f11c9fd59e397edb8da338d482d9de805f66
elloot <elliot.duchek@gmail.com>
2021-05-25 13:00:38 +0200

    catch exception thrown when client closes their connection

--- src/MouseInfoListener.java
+++ src/MouseInfoListener.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.net.SocketException;
 
 public class MouseInfoListener implements Runnable {
     private ObjectInputStream in;
@@ -26,6 +27,15 @@ public class MouseInfoListener implements Runnable {
             MouseInfo mouseInfo;
             try {
                 object = in.readObject();
+            } catch (SocketException e) {
+                if (e.getMessage().equals("Connection reset")) {
+                    System.out.println("Client disconnected, exiting");
+                    System.exit(0);
+                } else {
+                    System.out.println("Something went wrong with the connection to the client");
+                    e.printStackTrace();
+                }
+                object = null;
             } catch (IOException | ClassNotFoundException e) {
                 object = null;
                 System.out.println("Something went wrong when reading the mouse info");
